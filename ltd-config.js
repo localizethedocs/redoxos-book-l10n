be07dff66875efc37be47d522a529f7505296ac9
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-01 14:02:15 +0000

    Deploying to pages from @ localizethedocs/redoxos-book-l10n@cb1f37e1182a43db800a595353e733f9591d1b0c 🚀

---
+++ ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/redoxos-book-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/redoxos-book-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/redoxos-book-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/redoxos-book-l10n"],
+  ]
+};
